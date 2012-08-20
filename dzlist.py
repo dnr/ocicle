8952db5640f42f129309dd99fc90556069d91d1d
David Reiss <davidn@gmail.com>
2012-08-19 22:12:16 -0400

    image metadata to another file

--- dzlist.py
+++ dzlist.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-import os
+import os, json
 from xml.dom.minidom import parse
 
 os.chdir('images')
 
+data = []
 for fn in sorted(os.listdir('.')):
 	if not fn.endswith('.xml'):
 		continue
@@ -12,7 +13,16 @@ for fn in sorted(os.listdir('.')):
 	[img] = xml.getElementsByTagName('Image')
 	[size] = img.getElementsByTagName('Size')
 	ts = int(img.getAttribute('TileSize'))
-	o = int(img.getAttribute('Overlap'))
 	w = int(size.getAttribute('Width'))
 	h = int(size.getAttribute('Height'))
-	print "  {name: '%s', w: %d, h: %d, ts: %d, o: %d}" % (fn[:-4], w, h, ts, o)
+	data.append({
+		'name': fn[:-4],
+		'w': w,
+		'h': h,
+		'ts': ts,
+		})
+
+with open('images.js', 'w') as f:
+	f.write('window.IMAGES = ')
+	json.dump(data, f)
+	f.write(';\n')
